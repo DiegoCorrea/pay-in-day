3926f49ec7cd202a551639da6aab825ba25a1ed7
Diego Correa <diegocorrea.cc@gmail.com>
2017-10-08 17:35:17 -0300

    finalizando o cadastro basico

--- frontend/src/billingCycle/billingCycle.jsx
+++ frontend/src/billingCycle/billingCycle.jsx
@@ -11,7 +11,7 @@ import TabHeader from '../common/tab/tabHeader'
 import TabContent from '../common/tab/tabContent'
 import { selectTab, showTabs } from '../common/tab/tabActions'
 
-import { create } from './billingCycleActions'
+import { create, update, remove } from './billingCycleActions'
 import List from './billingCycleList'
 import Form from './billingCycleForm'
 
@@ -37,12 +37,17 @@ class BillingCycle extends Component {
                 <List/>
               </TabContent>
               <TabContent id='tabCreate'>
-                <Form onSubmit={this.props.create}/>
+                <Form onSubmit={this.props.create}
+                submitLabel='Incluir' submitClass='primary'/>
               </TabContent>
               <TabContent id='tabUpdate'>
-                <Form/>
+                <Form onSubmit={this.props.update}
+                  submitLabel='Alterar' submitClass='info'/>
               </TabContent>
-              <TabContent id='tabDelete'><h1>Excluir</h1></TabContent>
+              <TabContent id='tabDelete'>
+                <Form onSubmit={this.props.remove} readOnly={true}
+                  submitLabel='Excluir' submitClass='danger'/>
+              </TabContent>
             </TabsContent>
           </Tabs>
         </Content>
@@ -52,6 +57,6 @@ class BillingCycle extends Component {
 }
 
 const mapDispatchToProps = dispatch => bindActionCreators({
-  selectTab, showTabs, create
+  selectTab, showTabs, create, update, remove
 }, dispatch)
 export default connect(null, mapDispatchToProps)(BillingCycle)
