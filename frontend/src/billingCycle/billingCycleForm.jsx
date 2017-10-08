f3f1ff4e5e1a51527fd5846d5c20c9868f86d66a
Diego Correa <diegocorrea.cc@gmail.com>
2017-10-08 13:35:48 -0300

    adicionar e alterar funcionando com redux form

---
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import { reduxForm, Field } from 'redux-form'
+
+import labelAndInput from '../common/form/labelAndInput'
+import { init } from './billingCycleActions'
+
+class BillingCycleForm extends Component {
+  render() {
+    const { handleSubmit } = this.props
+    return (
+      <form role='form' onSubmit={handleSubmit}>
+        <div className='box-body'>
+          <Field name='name' component={labelAndInput}
+          label='Nome' cols='12 4' placeholder='Informe o nome' />
+          <Field name='month' component={labelAndInput}
+          type='number' label='Ano' cols='12 4' placeholder='Informe o ano' />
+          <Field name='year' component={labelAndInput}
+          label='Ano' cols='12 4' placeholder='Informe o ano' />
+        </div>
+        <div className='box-footer'>
+          <button type='submit' className='btn btn-primary'>Submit</button>
+          <button type='button' className='btn btn-default'
+            onClick={this.props.init}>Cancelar</button>
+        </div>
+      </form>
+    )
+  }
+}
+BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
+
+const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
+export default connect(null, mapDispatchToProps)(BillingCycleForm)
