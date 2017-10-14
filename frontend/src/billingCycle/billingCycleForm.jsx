fab6420ba8522d5070bf7904bfa88ab613c1d7c5
Diego Correa <diegocorrea.cc@gmail.com>
2017-10-14 11:40:01 -0300

    adicionando botao de deletar e generalizando o cmoponente

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -5,11 +5,11 @@ import { reduxForm, Field, formValueSelector } from 'redux-form'
 
 import labelAndInput from '../common/form/labelAndInput'
 import { init } from './billingCycleActions'
-import CreditList from './creditList'
+import ItemList from './itemList'
 
 class BillingCycleForm extends Component {
   render() {
-    const { handleSubmit, readOnly, credits } = this.props
+    const { handleSubmit, readOnly, credits, debts } = this.props
     return (
       <form role='form' onSubmit={handleSubmit}>
         <div className='box-body'>
@@ -19,7 +19,10 @@ class BillingCycleForm extends Component {
           type='number' label='Ano' cols='12 4' placeholder='Informe o ano' />
           <Field name='year' component={labelAndInput} readOnly={readOnly}
           label='Ano' cols='12 4' placeholder='Informe o ano' />
-          <CreditList cols='12 6' list={credits} readOnly={readOnly}/>
+          <ItemList cols='12 6' list={credits} readOnly={readOnly}
+            field='credits' legend='Créditos' />
+          <ItemList cols='12 6' list={debts} readOnly={readOnly}
+            field='debts' legend='Debitos' />
         </div>
         <div className='box-footer'>
           <button type='submit' className={`btn btn-${this.props.submitClass}`}>{this.props.submitLabel}</button>
@@ -32,6 +35,6 @@ class BillingCycleForm extends Component {
 }
 BillingCycleForm = reduxForm({form: 'billingCycleForm', destroyOnUnmount: false})(BillingCycleForm)
 const selector = formValueSelector('billingCycleForm')
-const mapStateToPros = state => ({credits: selector(state, 'credits')})
+const mapStateToPros = state => ({credits: selector(state, 'credits'), debts: selector(state, 'debts')})
 const mapDispatchToProps = dispatch => bindActionCreators({init}, dispatch)
 export default connect(mapStateToPros, mapDispatchToProps)(BillingCycleForm)
